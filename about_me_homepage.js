cee05002933fe4b3f3c6b188b8c06a747f427dc2
Andy Johnson <andy.johnson.1993@hotmail.co.uk>
2024-01-30 15:50:15 +0000

    Javascript added and minor visual tweaks to homepage

--- about_me_homepage.js
+++ about_me_homepage.js
@@ -1,19 +1,41 @@
 // the variables
+
 let introH3 = document.getElementsByClassName('intro')[0];
 let employmentH3 = document.getElementsByClassName('employment')[0];
 let hobbiesH3 = document.getElementsByClassName('hobbies')[0];
 
-let introP = document.getElementsByClassName('intro')[1];
-let employmentP = document.getElementsByClassName('employment')[1];
-let hobbiesP = document.getElementsByClassName('hobbies')[1];
+let introP = document.getElementById('intro');
+let employmentP = document.getElementById('employment');
+let hobbiesP = document.getElementById('hobbies');
 
 // the functions
+
+// ABOUT ME GRID
 
 introP.hidden = true;
 employmentP.hidden = true;
 hobbiesP.hidden = true;
 
-introH3.onclick = function() {
+introH3.onmouseover = function() {
     introP.hidden = false;
-}
+};
 
+introH3.onmouseout = function() {
+    introP.hidden = true;
+};
+
+employmentH3.onmouseover = function() {
+    employmentP.hidden = false;
+};
+
+employmentH3.onmouseout = function() {
+    employmentP.hidden = true;
+};
+
+hobbiesH3.onmouseover = function() {
+    hobbiesP.hidden = false;
+};
+
+hobbiesH3.onmouseout = function() {
+    hobbiesP.hidden = true;
+};
